48f539e2a77f32aa7617d3cd40aaeda3557b09f2
Paul <bujor.paul2000@gmail.com>
2020-09-16 21:57:09 +0200

    s2 done

---
+++ BirbWatchers/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BirbWatchers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<BirdWatcher> boomers = new List<BirdWatcher>();
+			Bird bird = new Bird();
+			Random rand = new Random();
+
+			for (int i = 0; i < 5; i++)
+			{
+				boomers.Add(new BirdWatcher(i, rand.Next() % 2 == 0 ? true : false));
+				bird.BirdAction += boomers[i].ReactToBird;
+			}
+
+			bird.Run();
+		}
+	}
+
+	class Bird
+	{
+		private string[] actions = { "flap", "sing", "fly" };
+		public Action<string> BirdAction;
+
+		public void Run()
+		{
+			Random rand = new Random();
+			for (int i = 0; i < 5; i++)
+			{
+				BirdAction?.Invoke(actions[rand.Next(actions.Length)]);
+				Thread.Sleep(1000);
+			}
+		}
+	}
+
+	class BirdWatcher
+	{
+		public bool IsDeaf { get; set; }
+		public int ID {get; set;}
+
+		public BirdWatcher(int i, bool isDeaf)
+		{
+			ID = i;
+			IsDeaf = isDeaf;
+		}
+
+		public void ReactToBird(string action)
+		{
+			Console.Write($"{ID}: ");
+			switch (action)
+			{
+				case "flap":
+					Console.Write("Wow it's flyin'");
+					break;
+				case "fly":
+					Console.Write("Goodbye");
+					break;
+				case "sing":
+					if (!IsDeaf) Console.Write("Bootiful");
+					break;
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ CarPredicates/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarPredicates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//conditions
+			string color = "Blue";
+
+			List<Car> cars = new List<Car>();
+			for (int i = 0; i < 5; i++)
+			{
+				cars.Add(Car.generateCar());
+				Console.WriteLine(cars[i]);
+			}
+
+			var blueCars = cars.FindAll(c => c.Color == color);
+
+			Console.WriteLine("______________");
+
+			foreach (Car car in blueCars)
+			{
+				Console.WriteLine(car);
+			}
+
+			var bigEngined = cars.FindAll(c => c.EngineSize >= 4);
+
+			Console.WriteLine("______________");
+
+			foreach (Car car in bigEngined)
+			{
+				Console.WriteLine(car);
+			}
+
+			var ecoBoxes = cars.FindAll(c => c.FuelEconomy <=4);
+
+			Console.WriteLine("______________");
+
+			foreach (Car car in ecoBoxes)
+			{
+				Console.WriteLine(car);
+			}
+
+			var manuals = cars.FindAll(c => c.IsManualShift);
+
+			Console.WriteLine("______________");
+
+			foreach (Car car in manuals)
+			{
+				Console.WriteLine(car);
+			}
+		}
+	}
+
+
+	class Car
+	{
+		public string Color { get; set; }
+		public double EngineSize { get; set; }
+		public double FuelEconomy { get; set; }
+		public bool IsManualShift { get; set; }
+
+		public Car(string color, double engineSize, double fuelEconomy, bool isManualShift)
+		{
+			Color = color;
+			EngineSize = engineSize;
+			FuelEconomy = fuelEconomy;
+			IsManualShift = isManualShift;
+		}
+
+		public override string ToString()
+		{
+			return $"{Color} {EngineSize: 0.00}L {FuelEconomy: 0.00}l/100km {(IsManualShift ? "Manual" : "Auto")}";
+		}
+
+		public static Car generateCar()
+		{
+			Random rand = new Random();
+			string[] colorSet = { "Blue", "Black", "Gray", "Yellow", "Green", "Red" };
+			string color = colorSet[rand.Next(colorSet.Length)];
+			double engineSize = rand.NextDouble() * 5;
+			double fuelEconomy = engineSize * 2;
+			bool isManual = rand.Next() % 2 == 0 ? true : false;
+			return new Car(color, engineSize, fuelEconomy, isManual);
+		}
+	}
+}
