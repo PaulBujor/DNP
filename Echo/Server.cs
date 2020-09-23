2019c4d9664428ad8ce6629c6db1afef3536b651
Paul <bujor.paul2000@gmail.com>
2020-09-23 20:26:16 +0200

    s3-3

--- Echo/Server.cs
+++ Echo/Server.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection.PortableExecutable;
@@ -9,38 +11,75 @@ namespace Echo
 {
 	class Server
 	{
+		private List<ClientHandler> clients;
+
+		public Action<string> OnMessageReceive;
+
 		static void Main(string[] args)
+		{
+			new Server().Run();
+		}
+
+		public Server()
+		{
+			clients = new List<ClientHandler>();
+		}
+
+		public void Run()
 		{
 			Console.WriteLine("Starting server...");
 
 			IPAddress ip = IPAddress.Parse("127.0.0.1");
-			TcpListener listener = new TcpListener(ip, 5000);
+			TcpListener listener = new TcpListener(ip, 6969);
 			listener.Start();
 
 			Console.WriteLine("Server started...");
+			OnMessageReceive += Send;
 
-			while(true)
+			while (true)
 			{
 				TcpClient client = listener.AcceptTcpClient();
-				Thread clientHandler = new Thread(new ClientHandler(client).Run);
-				clientHandler.Start();
+				ClientHandler clientHandler = new ClientHandler(client, OnMessageReceive);
+				clientHandler.ClientDisconnected += Disconnect;
+				clients.Add(clientHandler);
+
+				Thread clientThread = new Thread(clientHandler.Run);
+				clientThread.Start();
 			}
+		}
+
+		public void Send(string s)
+		{
+			foreach (ClientHandler client in clients)
+			{
+				client.Send(s);
+			}
+		}
+
+		public void Disconnect(ClientHandler client)
+		{
+			clients.Remove(client);
 		}
 	}
 
 	class ClientHandler
 	{
 		private TcpClient client;
+		private NetworkStream stream;
 
-		public ClientHandler(TcpClient client)
+		public Action<string> OnMessageReceive;
+		public Action<ClientHandler> ClientDisconnected;
+
+		public ClientHandler(TcpClient client, Action<String> action)
 		{
 			this.client = client;
+			stream = client.GetStream();
+			OnMessageReceive = action;
 		}
 
 		public void Run()
 		{
 			Console.WriteLine("Client connected");
-			NetworkStream stream = client.GetStream();
 
 			bool stayConnected = true;
 
@@ -52,24 +91,30 @@ namespace Echo
 					byte[] dataFromClient = new byte[1024];
 					int bytesRead = stream.Read(dataFromClient, 0, dataFromClient.Length);
 					string s = Encoding.ASCII.GetString(dataFromClient, 0, bytesRead);
-					Console.WriteLine(s);
+					OnMessageReceive?.Invoke(s);
 					stayConnected = !s.Equals("exit");
-
-					if (stayConnected)
-					{
-						//reply
-						byte[] dataToClient = Encoding.ASCII.GetBytes($"Reply: {s}");
-						stream.Write(dataToClient, 0, dataToClient.Length);
-					}
-				} catch (System.IO.IOException e)
+				}
+				catch (System.IO.IOException e)
 				{
-					Console.WriteLine("Client disconnected with force.");
+					Console.WriteLine("Client force disconnected.");
 					stayConnected = false;
 				}
 			}
+			Disconnect();
 
 			Console.WriteLine("Client disconnected...");
 			client.Close();
+		}
+
+		public void Send(String s)
+		{
+			byte[] dataToClient = Encoding.ASCII.GetBytes(s);
+			stream.Write(dataToClient, 0, dataToClient.Length);
+		}
+
+		public void Disconnect()
+		{
+			ClientDisconnected?.Invoke(this);
 		}
 	}
 }
--- EchoClient/Client.cs
+++ EchoClient/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace EchoClient
 {
@@ -10,24 +11,38 @@ namespace EchoClient
 		{
 			Console.WriteLine("Starting client...");
 
-			TcpClient client = new TcpClient("127.0.0.1", 5000);
+			TcpClient client = new TcpClient("localhost", 5000);
 
 			NetworkStream stream = client.GetStream();
 
+			bool connected = true;
+
 			string msg = "connect";
 
-			while (!msg.Equals("exit"))
+			new Thread(() =>
+			{
+				while (connected)
+				{
+					byte[] dataFromServer = new byte[1024];
+					int bytesRead = stream.Read(dataFromServer, 0, dataFromServer.Length);
+					string response = Encoding.ASCII.GetString(dataFromServer, 0, bytesRead);
+					Console.WriteLine(response);
+				}
+			}).Start();
+
+			while (connected)
 			{
 				msg = Console.ReadLine();
 
-				byte[] dataToServer = Encoding.ASCII.GetBytes(msg);
-				stream.Write(dataToServer, 0, dataToServer.Length);
-
-				byte[] dataFromServer = new byte[1024];
-				int bytesRead = stream.Read(dataFromServer, 0, dataFromServer.Length);
-				string response = Encoding.ASCII.GetString(dataFromServer, 0, bytesRead);
-				Console.WriteLine(response);
-
+				if (!msg.Equals("exit"))
+				{
+					byte[] dataToServer = Encoding.ASCII.GetBytes(msg);
+					stream.Write(dataToServer, 0, dataToServer.Length);
+				}
+				else
+				{
+					connected = false;
+				}
 			}
 
 			stream.Close();
